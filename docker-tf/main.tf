576e71746fa3eb2b349f9923cc61ad6945da7225
tftraining123 <tftraining@piercely.com>
2026-06-08 19:28:28 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  network_mode = "bridge"
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+#comment1
+//comment2
+/*comment3
+*/
+}
